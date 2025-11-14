55c2ae2b3dee8a41c9f1e08dc653d36d1439baaa
Coxxs <58-coxxs@users.noreply.git.ryujinx.app>
2025-11-15 03:46:25 +0800

    Check if Switch is running before creating minidump

--- src/Ryujinx/Program.cs
+++ src/Ryujinx/Program.cs
@@ -382,14 +382,14 @@ namespace Ryujinx.Ava
                 exceptions.Add(initialException);
             }
 
-            if (isTerminating)
+            if (isTerminating && HLE.Switch.Shared is { } device)
             {
                 try
                 {
                     // Print a short message first just in case it crashes again during minidump creation (should not happen)
                     Logger.Error?.Print(LogClass.Application, $"Unhandled exception caught: {initialException.GetType().Name}. Creating guest program minidump...");
 
-                    var minidump = HLE.Switch.Shared?.System?.DebugGetApplicationProcessMinidump();
+                    var minidump = device.System?.DebugGetApplicationProcessMinidump();
 
                     if (minidump == null)
                     {
