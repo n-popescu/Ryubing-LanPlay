928a189d997e76c30a05b0f6ca81eb688445ddb8
_Neo_ <ursamajorjanus2819@gmail.com>
2025-11-17 13:43:23 +0200

    Fix "No Profiles To Recover" not updating in certain instances
    "No Profiles To Recover" text did not show up after saving the recovered profile, as the user is still on the "Profile Recovery" window. This now updates this.

--- src/Ryujinx/UI/ViewModels/UserProfileViewModel.cs
+++ src/Ryujinx/UI/ViewModels/UserProfileViewModel.cs
@@ -1,7 +1,7 @@
 using Ryujinx.Ava.UI.Models;
 using System;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.Collections.Specialized;
 
 namespace Ryujinx.Ava.UI.ViewModels
 {
@@ -11,23 +11,32 @@ namespace Ryujinx.Ava.UI.ViewModels
         {
             Profiles = new ObservableCollection<BaseModel>();
             LostProfiles = new ObservableCollection<UserProfile>();
+            LostProfiles.CollectionChanged += LostProfilesChanged;
         }
 
-        public ObservableCollection<BaseModel> Profiles { get; set; }
+        public ObservableCollection<BaseModel> Profiles { get; }
 
-        public ObservableCollection<UserProfile> LostProfiles { get; set; }
+        public ObservableCollection<UserProfile> LostProfiles { get; }
 
-        public bool IsEmpty => !LostProfiles.Any();
+        public bool IsEmpty => LostProfiles.Count == 0;
 
         public void Dispose()
         {
-            GC.SuppressFinalize(this);
+            LostProfiles.CollectionChanged -= LostProfilesChanged;
+        }
+
+        private void LostProfilesChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(IsEmpty));
         }
 
         public void UpdateLostProfiles(ObservableCollection<UserProfile> newProfiles)
         {
-            LostProfiles = newProfiles;
-            OnPropertyChanged(nameof(LostProfiles));
+            LostProfiles.Clear();
+            
+            foreach (var profile in newProfiles)
+                LostProfiles.Add(profile);
+
             OnPropertyChanged(nameof(IsEmpty));
         }
     }
