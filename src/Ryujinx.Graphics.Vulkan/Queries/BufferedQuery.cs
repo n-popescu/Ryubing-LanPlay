48adca40f54415671f63ae501d4e470ad6005c40
Martin Bai <bxymartin@hotmail.com>
2025-12-25 13:47:27 +0000

    Add max retries to BufferedQuery regardless of the presence of wakeSignal, fixed freezing at character selection screen of Super Mario Party Jamboree

--- src/Ryujinx.Graphics.Vulkan/Queries/BufferedQuery.cs
+++ src/Ryujinx.Graphics.Vulkan/Queries/BufferedQuery.cs
@@ -145,28 +145,13 @@ namespace Ryujinx.Graphics.Vulkan.Queries
         {
             long data = _defaultValue;
 
-            if (wakeSignal == null)
+            int iterations = 0;
+            while (WaitingForValue(data) && iterations++ < MaxQueryRetries)
             {
-                while (WaitingForValue(data))
+                data = Marshal.ReadInt64(_bufferMap);
+                if (wakeSignal != null && WaitingForValue(data))
                 {
-                    data = Marshal.ReadInt64(_bufferMap);
-                }
-            }
-            else
-            {
-                int iterations = 0;
-                while (WaitingForValue(data) && iterations++ < MaxQueryRetries)
-                {
-                    data = Marshal.ReadInt64(_bufferMap);
-                    if (WaitingForValue(data))
-                    {
-                        wakeSignal.WaitOne(1);
-                    }
-                }
-
-                if (iterations >= MaxQueryRetries)
-                {
-                    Logger.Error?.Print(LogClass.Gpu, $"Error: Query result {_type} timed out. Took more than {MaxQueryRetries} tries.");
+                    wakeSignal.WaitOne(0);
                 }
             }
 
