9e224b73b156b1caaee0628e5a5d051189300d3d
Shyanne <randomgirlisweird@gmail.com>
2026-03-14 09:51:29 -0400

    rider moment

--- src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
+++ src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
@@ -14,21 +14,5 @@ namespace Ryujinx.HLE.HOS.Services.Notification
             context.ResponseData.Write(alarmSettingsCount);
             return ResultCode.Success;
         }
-
-        [CommandCmif(1000)] // 9.0.0+
-        // GetNotificationCount() -> nn::notification::server::INotificationSystemEventAccessor
-        public ResultCode GetNotificationCount(ServiceCtx context)
-        {
-            MakeObject(context, new INotificationSystemEventAccessor(context));
-            return ResultCode.Success;
-        }
-        
-        [CommandCmif(1040)] // 9.0.0+
-        // GetNotificationSendingNotifier() -> nn::notification::server::INotificationSystemEventAccessor
-        public ResultCode GetNotificationSendingNotifier(ServiceCtx context)
-        {
-            MakeObject(context, new INotificationSystemEventAccessor(context));
-            return ResultCode.Success;
-        }
     }
 }
