33d133e2ac2e5f9b5ba6ee89a2042372f2d3fd60
Shyanne <randomgirlisweird@gmail.com>
2026-03-14 02:40:18 -0400

    [HLE] Renamed INotificationServicesForSystem and implemented a commands (and stubs)
    Should allow Ring Fit to get in-game, needs testing.

--- src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
+++ src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
@@ -4,5 +4,31 @@ namespace Ryujinx.HLE.HOS.Services.Notification
     class INotificationServicesForApplication : IpcService
     {
         public INotificationServicesForApplication(ServiceCtx context) { }
+        
+        [CommandCmif(520)] // 9.0.0+
+        // ListAlarmSettings(nn::arp::ApplicationCertificate) -> s32 AlarmSettingsCount
+        public ResultCode ListAlarmSettings(ServiceCtx context)
+        {
+            // TO-DO: Currently just returns 0. Should read in an ApplicationCertificate.
+            int alarmSettingsCount = 0;
+            context.ResponseData.Write(alarmSettingsCount);
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(1000)] // 9.0.0+
+        // GetNotificationCount() -> nn::notification::server::INotificationSystemEventAccessor
+        public ResultCode GetNotificationCount(ServiceCtx context)
+        {
+            MakeObject(context, new INotificationSystemEventAccessor(context));
+            return ResultCode.Success;
+        }
+        
+        [CommandCmif(1040)] // 9.0.0+
+        // GetNotificationSendingNotifier() -> nn::notification::server::INotificationSystemEventAccessor
+        public ResultCode GetNotificationSendingNotifier(ServiceCtx context)
+        {
+            MakeObject(context, new INotificationSystemEventAccessor(context));
+            return ResultCode.Success;
+        }
     }
 }
