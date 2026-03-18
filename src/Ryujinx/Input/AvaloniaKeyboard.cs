aa7ec6da654f6430289fe93e4c14fd0d7cc18d5e
Babib3l <gab.chevanne@gmail.com>
2026-03-18 20:21:24 +0100

    Cache Avalonia keyboard LED state

--- src/Ryujinx/Input/AvaloniaKeyboard.cs
+++ src/Ryujinx/Input/AvaloniaKeyboard.cs
@@ -16,6 +16,7 @@ namespace Ryujinx.Ava.Input
         private readonly AvaloniaKeyboardDriver _driver;
         private readonly KeyboardInputMode _mode;
         private StandardKeyboardInputConfig _configuration;
+        private uint _ledValue;
 
         private readonly Lock _userMappingLock = new();
 
@@ -125,6 +126,13 @@ namespace Ryujinx.Ava.Input
 
         public void SetLed(uint packedRgb)
         {
+            if (_ledValue == packedRgb)
+            {
+                return;
+            }
+
+            _ledValue = packedRgb;
+
             Logger.Debug?.Print(LogClass.UI, "SetLed called on an AvaloniaKeyboard");
         }
 
