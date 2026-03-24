bb687b6523c12cfa617962580adb6ba00264adc3
_Neo_ <ursamajorjanus2819@gmail.com>
2026-03-24 21:49:55 +0200

    Remove Cheat Manager from Actions entirely

--- src/Ryujinx/UI/ViewModels/MainWindowViewModel.cs
+++ src/Ryujinx/UI/ViewModels/MainWindowViewModel.cs
@@ -1886,24 +1886,6 @@ namespace Ryujinx.Ava.UI.ViewModels
                 Title = RyujinxApp.FormatTitle();
             });
         }
-        
-        public async Task OpenCheatManagerForCurrentApp()
-        {
-            if (IsGameRunning)
-            {
-                string name = AppHost.Device.Processes.ActiveApplication.ApplicationControlProperties.Title[(int)AppHost.Device.System.State.DesiredTitleLanguage].NameString.ToString();
-
-                await StyleableAppWindow.ShowAsync(
-                    new CheatWindow(
-                        Window.VirtualFileSystem,
-                        AppHost.Device.Processes.ActiveApplication.ProgramIdText,
-                        name,
-                        SelectedApplication.Path)
-                );
-
-                AppHost.Device.EnableCheats();
-            }
-        }
 
         public async Task OpenAmiiboWindow()
         {
