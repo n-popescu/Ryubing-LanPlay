9c1ed4e2bcf6340a62be097eca407de4b2c3a156
Babib3l <gab.chevanne@gmail.com>
2026-04-24 15:33:48 +0200

    Fix Windows console hide race

--- src/Ryujinx.Common/Helpers/ConsoleHelper.cs
+++ src/Ryujinx.Common/Helpers/ConsoleHelper.cs
@@ -2,11 +2,14 @@ using Ryujinx.Common.Logging;
 using System;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
+using System.Threading;
 
 namespace Ryujinx.Common.Helper
 {
     public static partial class ConsoleHelper
     {
+        private static int _windowStateRequestId;
+
         [SupportedOSPlatform("windows")]
         [LibraryImport("kernel32")]
         private static partial nint GetConsoleWindow();
@@ -16,13 +19,18 @@ namespace Ryujinx.Common.Helper
         [return: MarshalAs(UnmanagedType.Bool)]
         private static partial bool ShowWindow(nint hWnd, int nCmdShow);
 
+        [SupportedOSPlatform("windows")]
+        [LibraryImport("user32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool IsWindowVisible(nint hWnd);
+
         public static bool SetConsoleWindowStateSupported => OperatingSystem.IsWindows();
 
         public static void SetConsoleWindowState(bool show)
         {
             if (OperatingSystem.IsWindows())
             {
-                SetConsoleWindowStateWindows(show);
+                SetConsoleWindowStateWindows(show, Interlocked.Increment(ref _windowStateRequestId));
             }
             else if (show == false)
             {
@@ -31,7 +39,47 @@ namespace Ryujinx.Common.Helper
         }
 
         [SupportedOSPlatform("windows")]
-        private static void SetConsoleWindowStateWindows(bool show)
+        private static void SetConsoleWindowStateWindows(bool show, int requestId)
+        {
+            if (TrySetConsoleWindowStateWindows(show))
+            {
+                return;
+            }
+
+            if (show)
+            {
+                Logger.Warning?.Print(LogClass.Application, "Attempted to show/hide console window but console window does not exist");
+                return;
+            }
+
+            ThreadPool.QueueUserWorkItem(static state =>
+            {
+                int queuedRequestId = (int)state!;
+
+                for (int attempt = 0; attempt < 10; attempt++)
+                {
+                    Thread.Sleep(50);
+
+                    if (queuedRequestId != Volatile.Read(ref _windowStateRequestId))
+                    {
+                        return;
+                    }
+
+                    if (TrySetConsoleWindowStateWindows(false))
+                    {
+                        return;
+                    }
+                }
+
+                if (queuedRequestId == Volatile.Read(ref _windowStateRequestId))
+                {
+                    Logger.Warning?.Print(LogClass.Application, "Attempted to hide console window but it did not become hidden");
+                }
+            }, requestId);
+        }
+
+        [SupportedOSPlatform("windows")]
+        private static bool TrySetConsoleWindowStateWindows(bool show)
         {
             const int SW_HIDE = 0;
             const int SW_SHOW = 5;
@@ -40,11 +88,12 @@ namespace Ryujinx.Common.Helper
 
             if (hWnd == nint.Zero)
             {
-                Logger.Warning?.Print(LogClass.Application, "Attempted to show/hide console window but console window does not exist");
-                return;
+                return false;
             }
 
             ShowWindow(hWnd, show ? SW_SHOW : SW_HIDE);
+
+            return IsWindowVisible(hWnd) == show;
         }
     }
 }
