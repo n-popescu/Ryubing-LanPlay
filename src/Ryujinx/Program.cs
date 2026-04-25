3695186a4dfb30c78ec1d82c246d68d094fa8dfd
Awesomeangotti <anchovieindustries@gmail.com>
2026-04-25 17:31:31 -0400

    Revise naming conventions

--- src/Ryujinx/Program.cs
+++ src/Ryujinx/Program.cs
@@ -438,7 +438,7 @@ namespace Ryujinx.Ava
         internal static void PrintSystemInfo()
         {
             // Print the ryubing logo + joke splash
-            StartupTextHelper.MakeSplash(); 
+            StartupTextHelper.PrintSplash(); 
             
             Logger.Notice.Print(LogClass.Application, $"{RyujinxApp.FullAppName} Version: {Version}");
             Logger.Notice.Print(LogClass.Application, $".NET Runtime: {RuntimeInformation.FrameworkDescription}");
