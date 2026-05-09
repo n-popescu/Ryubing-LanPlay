43a5b877b6292e987cb59ebce90fad16bb9a4a36
Max <randomgirlisweird@gmail.com>
2026-05-08 22:04:46 -0400

    re-populated activeStages (don't code on 2 hours of sleep kids)

--- src/Ryujinx.Graphics.Vulkan/PipelineLayoutFactory.cs
+++ src/Ryujinx.Graphics.Vulkan/PipelineLayoutFactory.cs
@@ -17,11 +17,21 @@ namespace Ryujinx.Graphics.Vulkan
             DescriptorSetLayout[] layouts = new DescriptorSetLayout[setDescriptors.Count];
             bool[] updateAfterBindFlags = new bool[setDescriptors.Count];
             
+            bool isMoltenVk = gd.IsMoltenVk;
+            
             for (int setIndex = 0; setIndex < setDescriptors.Count; setIndex++)
             {
                 ResourceDescriptorCollection rdc = setDescriptors[setIndex];
 
                 ResourceStages activeStages = ResourceStages.None;
+                
+                if (isMoltenVk)
+                {
+                    for (int descIndex = 0; descIndex < rdc.Descriptors.Count; descIndex++)
+                    {
+                        activeStages |= rdc.Descriptors[descIndex].Stages;
+                    }
+                }
 
                 DescriptorSetLayoutBinding[] layoutBindings = new DescriptorSetLayoutBinding[rdc.Descriptors.Count];
 
@@ -34,11 +44,11 @@ namespace Ryujinx.Graphics.Vulkan
 
                     if (descriptor.Type == ResourceType.StorageBuffer && gd.IsMoltenVk)
                     {
-                        // There's a bug on MoltenVK where using the same buffer across different stages
+                        // There's a bug in MoltenVK where using the same buffer across different stages
                         // causes invalid resource errors, allow the binding on all active stages as workaround.
                         // https://github.com/KhronosGroup/MoltenVK/issues/1870
                         stages = activeStages;
-                    }
+                    }   
 
                     layoutBindings[descIndex] = new DescriptorSetLayoutBinding
                     {
