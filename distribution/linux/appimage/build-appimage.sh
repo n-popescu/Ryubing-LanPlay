72afc0a1d515ce882846b106746f4e34d130846c
ranidspace <dev@ranid.space>
2026-05-21 16:44:33 -0400

    Fix builds, follow AppStream guidelines further
    Adds back the -n flag. In the future this can be removed if the website
    is back up.
    
    Rearrange yet MORE files to follow AppStream guidelines, like the proper
    location of the .desktop and icon files. As well as adding bits of
    metadata to remove warnings

--- distribution/linux/appimage/build-appimage.sh
+++ distribution/linux/appimage/build-appimage.sh
@@ -7,16 +7,23 @@ cd "$ROOTDIR"
 BUILDDIR=${BUILDDIR:-publish}
 OUTDIR=${OUTDIR:-publish_appimage}
 
+# AppStream
+
 rm -rf AppDir
-mkdir -p AppDir/usr/lib AppDir/usr/bin AppDir/usr/share/metainfo
+mkdir -p AppDir/usr/{lib,bin} AppDir/usr/share/{metainfo,applications}
+mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps/
 
 cp -r "$BUILDDIR"/* AppDir/usr/lib/
 
 cp distribution/linux/appimage/app.ryujinx.Ryujinx.appdata.xml AppDir/usr/share/metainfo/app.ryujinx.Ryujinx.appdata.xml
-cp distribution/linux/app.ryujinx.Ryujinx.desktop AppDir/app.ryujinx.Ryujinx.desktop
-cp distribution/misc/Logo.png AppDir/app.ryujinx.Ryujinx.png
+cp distribution/linux/app.ryujinx.Ryujinx.desktop AppDir/usr/share/applications/app.ryujinx.Ryujinx.desktop
+cp distribution/misc/Logo.png AppDir/usr/share/icons/hicolor/256x256/apps/app.ryujinx.Ryujinx.png
 
-ln -s ./app.ryujinx.Ryujinx.png AppDir/.DirIcon
+# AppImage Root
+
+ln -s ./usr/share/applications/app.ryujinx.Ryujinx.desktop AppDir/app.ryujinx.Ryujinx.desktop
+ln -s ./usr/share/icons/hicolor/256x256/apps/app.ryujinx.Ryujinx.png AppDir/.DirIcon
+ln -s ./usr/share/icons/hicolor/256x256/apps/app.ryujinx.Ryujinx.png AppDir/app.ryujinx.Ryujinx.png
 ln -s ../lib/Ryujinx AppDir/usr/bin/Ryujinx
 ln -s ./usr/lib/Ryujinx.sh AppDir/AppRun
 
@@ -25,5 +32,7 @@ chmod +x AppDir/AppRun AppDir/usr/bin/Ryujinx*
 
 mkdir -p "$OUTDIR"
 
-appimagetool --appimage-extract-and-run --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 21 \
+# The "-n" flag removes the appstream checks during build, in case the main website is down.
+# Run "appstreamcli validate --explain AppDir/usr/share/metainfo/app.ryujinx.Ryujinx.appdata.xml" to check manually
+appimagetool --appimage-extract-and-run -n --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 21 \
     AppDir "$OUTDIR"/Ryujinx.AppImage
