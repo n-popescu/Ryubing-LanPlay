1076c3ba6b9bf48a8568ac8d71af3989bb63fed9
_Neo_ <ursamajorjanus2819@gmail.com>
2026-06-25 16:13:35 +0300

    More Comments

--- src/Ryujinx.Cpu/AppleHv/HvExecutionContext.cs
+++ src/Ryujinx.Cpu/AppleHv/HvExecutionContext.cs
@@ -226,6 +226,8 @@ namespace Ryujinx.Cpu.AppleHv
             HvVcpuPool.Instance.Destroy(vcpu, SwapContext);
         }
 
+        // TryGpuSync() is called periodically in the main Execute() loop. The "syncing" value can be tuned based on gameplay results.
+        // This feature it to be followed-up and further completed in a future PR.
         private void TryGpuSync()
         {
             try
@@ -269,7 +271,7 @@ namespace Ryujinx.Cpu.AppleHv
                     ReturnToPool(vcpu);
                     ushort id = (ushort)esr;
                     SupervisorCallHandler(elr - 4UL, id);
-                    Thread.Yield();                    // Helps with Vulkan + Hypervisor stability
+                    Thread.Yield(); // MoltenVK causes extremely frequent SVC exits, and HVF handles them in a busy loop. Hypervisor.Framework accelerates the guest CPU, and without periodic yielding/flushing, MoltenVK's presentation queue can starve, causing permanent 0 FPS deadlock.
                     vcpu = RentFromPool(memoryManager.AddressSpace, vcpu);
                     break;
 
--- src/Ryujinx.Cpu/AppleHv/HvExecutionContextVcpu.cs
+++ src/Ryujinx.Cpu/AppleHv/HvExecutionContextVcpu.cs
@@ -43,6 +43,8 @@ namespace Ryujinx.Cpu.AppleHv
 
         static HvExecutionContextVcpu()
         {
+            // .NET does not support passing vectors by value, so we need to pass a pointer and use a native			
+            // function to load the value into a vector register.
             _setSimdFpRegFuncMem = new MemoryBlock(MemoryBlock.GetPageSize());
             _setSimdFpRegFuncMem.Write(0, 0x3DC00040u); // LDR Q0, [X2]
             _setSimdFpRegFuncMem.Write(4, 0xD61F0060u); // BR X3
