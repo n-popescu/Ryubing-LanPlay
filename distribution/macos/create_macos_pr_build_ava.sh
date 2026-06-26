00636df807c2e6021a5c8c2a13c1cd524d897916
Max <randomgirlisweird@gmail.com>
2026-06-25 23:48:33 -0500

    strace.

--- distribution/macos/create_macos_pr_build_ava.sh
+++ distribution/macos/create_macos_pr_build_ava.sh
@@ -147,7 +147,7 @@ dd if=/dev/zero of="$UNCOMPRESSED_DMG" bs=1M count=100
 mkfs.hfsplus -v "Ryujinx" "$UNCOMPRESSED_DMG"
 
 mkdir "$MOUNTED_DMG"
-sudo mount -v -t hfsplus -o loop "$UNCOMPRESSED_DMG" "$MOUNTED_DMG"
+sudo strace mount -t hfsplus -o loop "$UNCOMPRESSED_DMG" "$MOUNTED_DMG"
 sudo cp -r -a "$DMG_FOLDER/" "$MOUNTED_DMG"
 sudo umount "$MOUNTED_DMG"
 rm -r "$DMG_FOLDER"
