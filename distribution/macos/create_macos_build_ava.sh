4844e85fffc344836e09ee4a502b205987537063
Max <randomgirlisweird@gmail.com>
2026-07-07 08:09:38 -0500

    attempt custom icon attribute #2

--- distribution/macos/create_macos_build_ava.sh
+++ distribution/macos/create_macos_build_ava.sh
@@ -150,7 +150,9 @@ dd if=/dev/zero of="$UNCOMPRESSED_DMG" bs=1M count=100
 genisoimage -D -V "Ryujinx" \
     -no-pad -apple -uid 0 -gid 0 -dir-mode 0755 -file-mode 0755 \
     -o "$UNCOMPRESSED_DMG" "$DMG_FOLDER"
-hfsplus setattr "$UNCOMPRESSED_DMG" "kHasCustomIcon" 0x0400
+# https://developer.apple.com/library/archive/technotes/tn/tn1150.html
+FINDER_INFO=$(hfsplus "$UNCOMPRESSED_DMG" getattr / finderinfo)
+hfsplus "$UNCOMPRESSED_DMG" setattr / finderinfo ${FINDER_INFO:0:24}04${FINDER_INFO:26}
 dmg dmg -c lzma "$UNCOMPRESSED_DMG" "$COMPRESSED_DMG"
 rm -r "$DMG_FOLDER"
 rm -f "$UNCOMPRESSED_DMG"
