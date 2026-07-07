0b9a6ef5535e461e9d0c9dcda9c0c1a88ee2789a
Max <randomgirlisweird@gmail.com>
2026-07-07 09:49:37 -0500

    attempt custom icon attribute #5

--- distribution/macos/create_macos_pr_build_ava.sh
+++ distribution/macos/create_macos_pr_build_ava.sh
@@ -146,10 +146,7 @@ dd if=/dev/zero of="$UNCOMPRESSED_DMG" bs=1M count=100
 mkfs.hfsplus -v "Ryujinx" -J "$UNCOMPRESSED_DMG"
 
 shopt -s dotglob
-for file in "$DMG_FOLDER"/*; do
-    filename=$(basename "$file")
-    libdmg-hfsplus "$UNCOMPRESSED_DMG" add "/$filename" "$file" 0755
-done
+libdmg-hfsplus "$UNCOMPRESSED_DMG" addall / "$DMG_FOLDER"/*
 shopt -u dotglob
 
 # https://developer.apple.com/library/archive/technotes/tn/tn1150.html
