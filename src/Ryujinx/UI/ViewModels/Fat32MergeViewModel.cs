608696c3e1c6a467d67abbbb9238fbb973c32f1a
awesomeanchovies <anchovieindustries@gmail.com>
2026-07-30 14:02:35 -0400

    The button now opens the folder picker

--- src/Ryujinx/UI/ViewModels/Fat32MergeViewModel.cs
+++ src/Ryujinx/UI/ViewModels/Fat32MergeViewModel.cs
@@ -1,6 +1,7 @@
 
 using Avalonia.Data;
 using Avalonia.Platform.Storage;
+using Ryujinx.Ava.Utilities;
 using System;
 
 namespace Ryujinx.Ava.UI.ViewModels
@@ -12,19 +13,15 @@ namespace Ryujinx.Ava.UI.ViewModels
             Console.WriteLine("Deez Nuts");
         }
 
-        public void OpenFolderPicker() // Does not work right now. :angry_cat:
-        {
-            //Optional<IStorageFolder> folder = StorageProviderExtensions.Extension.OpenSingleFolderPickerAsync();
-        }
-
         public void MergeDump()
         {
+            RyujinxApp.MainWindow.ViewModel.StorageProvider.OpenSingleFolderPickerAsync();
             Console.WriteLine("Not implemented yet you silly person!"); // You know what this does. Make some command stuff that merges the files or something
         }
 
         public void SetProgress()
         {
-            Console.WriteLine("Not implemented yet you silly person!"); // Related to XCITrimmerViewModel.cs > SetProgress()
+            Console.WriteLine("Updating not implemented"); // Related to XCITrimmerViewModel.cs > SetProgress()
         }
     }
 }
--- src/Ryujinx/UI/Views/Dialog/Fat32MergeView.axaml.cs
+++ src/Ryujinx/UI/Views/Dialog/Fat32MergeView.axaml.cs
@@ -1,12 +1,16 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using Avalonia.Styling;
 using FluentAvalonia.UI.Controls;
+using Gommon;
 using Ryujinx.Ava.Common.Locale;
 using Ryujinx.Ava.Common.Models;
 using Ryujinx.Ava.UI.Controls;
 using Ryujinx.Ava.UI.ViewModels;
+using Ryujinx.Ava.Utilities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Ryujinx.Ava.UI.Views.Dialog
@@ -50,6 +54,20 @@ namespace Ryujinx.Ava.UI.Views.Dialog
         private void SetProgress(object sender, RoutedEventArgs e)
         {
             ViewModel.SetProgress();
+        }
+
+        private async void OpenFolderPicker(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Optional<IStorageFolder> folder = await RyujinxApp.MainWindow.ViewModel.StorageProvider.OpenSingleFolderPickerAsync();
+                Console.Write(folder.ToString());
+            }
+            catch (Exception exception)
+            {
+                // Put some logging here later using the logging stuff!
+            }
+            
         }
         
         private void Close(object sender, RoutedEventArgs e)
